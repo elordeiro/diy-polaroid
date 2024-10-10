0532864ac66aa4703fabeefe6f355d8f1816f252
elordeiro <estevao.lordeiro@hotmail.com>
2024-10-09 21:36:12 -0500

    Initial Commit

---
+++ go.mod
@@ -0,0 +1,13 @@
+module github.com/elordeiro/diy-polaroid
+
+go 1.23
+
+require (
+	github.com/disintegration/imaging v1.6.2
+	github.com/fogleman/gg v1.3.0
+)
+
+require (
+	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
+	golang.org/x/image v0.0.0-20191009234506-e7c1f5e7dbb8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"fmt"
+	"image"
+	"image/color"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/disintegration/imaging"
+	"github.com/fogleman/gg"
+)
+
+const (
+	canvasWidth   = 1200
+	canvasHeight  = 1800
+	lineThickness = 5
+	linePositionY = 1440
+	squareSize    = 1012 // 3.37x3.37 inches at 300 DPI
+	border        = 94   // 0.25-inch border on all sides
+)
+
+func main() {
+	// Get the path of the images
+	var path string
+	fmt.Print("Enter the path of the images: ")
+	fmt.Scanln(&path)
+
+	// Create the polaroids directory
+	polaroidDir := filepath.Join(path, "polaroids")
+	err := os.MkdirAll(polaroidDir, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Failed to create polaroid directory: %v", err)
+	}
+	fmt.Println("Polaroid directory created")
+
+	// Get the list of image files
+	files, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatalf("Failed to read directory: %v", err)
+	}
+
+	for _, file := range files {
+		// Filter image files
+		if file.IsDir() || !(filepath.Ext(file.Name()) == ".jpg" || filepath.Ext(file.Name()) == ".jpeg" || filepath.Ext(file.Name()) == ".png") {
+			continue
+		}
+
+		imagePath := filepath.Join(path, file.Name())
+		img, err := imaging.Open(imagePath, imaging.AutoOrientation(true))
+		if err != nil {
+			log.Printf("Error opening image %s: %v", file.Name(), err)
+			continue
+		}
+
+		// Crop image to square if necessary
+		img = cropToSquare(img)
+
+		// Resize the image
+		img = imaging.Resize(img, squareSize, squareSize, imaging.Lanczos)
+
+		// Create a gg context for the canvas
+		dc := gg.NewContext(canvasWidth, canvasHeight)
+
+		// Fill the canvas with white
+		dc.SetColor(color.White)
+		dc.Clear()
+
+		// Draw the image onto the canvas at the specified offset
+		dc.DrawImage(img, border, border)
+
+		// Draw the black cutting line
+		drawLine(dc)
+
+		// Export the final image
+		outputImage := dc.Image()
+		outputPath := filepath.Join(polaroidDir, file.Name())
+		err = imaging.Save(outputImage, outputPath)
+		if err != nil {
+			log.Printf("Error saving polaroid for %s: %v", file.Name(), err)
+			continue
+		}
+		fmt.Printf("Polaroid created for: %s\n", file.Name())
+	}
+}
+
+// cropToSquare crops an image to be a square
+func cropToSquare(img image.Image) image.Image {
+	width := img.Bounds().Dx()
+	height := img.Bounds().Dy()
+
+	if width > height {
+		offset := (width - height) / 2
+		return imaging.Crop(img, image.Rect(offset, 0, offset+height, height))
+	} else if height > width {
+		offset := (height - width) / 2
+		return imaging.Crop(img, image.Rect(0, offset, width, offset+width))
+	}
+	return img
+}
+
+// drawLine draws a horizontal line across the image canvas at a specific Y position
+func drawLine(dc *gg.Context) {
+	dc.SetRGB(0, 0, 0) // Set color to black
+	dc.SetLineWidth(lineThickness)
+	dc.MoveTo(0, linePositionY)           // Starting point of the line
+	dc.LineTo(canvasWidth, linePositionY) // End point of the line
+	dc.Stroke()                           // Apply the line drawing
+}
